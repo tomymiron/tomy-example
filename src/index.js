dbe9d155b78eeac2d1388c87aee7a13933b470a8
Tomymiron <brickshalk@gmail.com>
2023-06-19 22:35:59 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ app.get("/about", (req, res) => {
   res.send("<h1>This is the about</h1>");
 });
 
-app.listen(3000);
-console.log("Server on port 3000");
+app.listen(process.env);
+console.log("Server on port " + process.env);
